b5efb5b50b48c23a580c513c1e1fb445c6b9810e
Likitha Rapuri <61692499+Likitha640@users.noreply.github.com>
2024-05-18 19:39:34 +0530

    Add files via upload

---
+++ Data Types.py
@@ -0,0 +1,45 @@
+#Text Data Type - str
+a = "Hello World"
+print(a)
+
+#Numeric Data Type - int, float, complex
+b = 20
+print(b)
+c = 20.5
+print(c)
+d = 1j
+print(d)
+
+#Sequences Data Type - list, tuple, range
+e = ["apple", "banana", "cherry"]
+f = ("apple", "banana", "cherry")
+g = range(6)
+print(e)
+print(f)
+print(g)
+
+#Mapping Data Type - dict
+h = {"name" : "John", "age" : 36}
+print(h)
+
+#Set Types- set, frozenset
+i = {"apple", "banana", "cherry"}
+j = frozenset({"apple", "banana", "cherry"})
+print(i)
+print(j)
+
+#Boolean Data Type - bool
+k = True
+print(k)
+
+#Binary Data Type - bytes, bytearray,memoryview
+l = b"Hello"
+m = bytearray(5)
+n = memoryview(bytes(5))
+print(l)
+print(m)
+print(n)
+
+#None Data Type -NoneType
+o = None
+print(o)
