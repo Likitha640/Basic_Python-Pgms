a4d07ecb939a023fc65acb617b1936b663f7c034
Likitha Rapuri <61692499+Likitha640@users.noreply.github.com>
2024-05-20 20:09:14 +0530

    Add files via upload

---
+++ Python Strings.py
@@ -0,0 +1,54 @@
+#Python Strings
+#Single and Dobule Quotes
+
+print("Hello")
+print('Hello')
+
+#Quotes in Quotes
+print("I am Fine")
+print("Python is 'Good'")
+print('Python is "Good"')
+
+#Assign string to variable
+a="Python"
+print(a)
+
+#Variables with dobule and triple quotation
+b="""Python is an interpreted, object-oriented,
+high-level programming language
+with dynamic semantics"""
+print(b)
+
+c='''Python is an interpreted, object-oriented,
+high-level programming language
+with dynamic semantics'''
+print(c)
+
+#Strings with Array
+x = "Hello, World!"
+print(x[1])
+
+#Looping through a Strings
+for y in "banana":
+  print(y)
+
+#String length
+z = "Hello, World"
+print(len(z))
+
+#Check String
+str = "Python is good"
+print("good" in str)
+print("bad" in str)
+
+#if statement
+if "good" in str:
+  print("Yes, 'good' is present.")
+#Check if Not
+if "bad" not in str:
+  print("No, 'bad' is NOT present.")
+
+
+
+
+
