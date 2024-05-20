b78b8bbe1e69ab64413f7b4da98109b0c4e35caa
Likitha Rapuri <61692499+Likitha640@users.noreply.github.com>
2024-05-20 19:46:37 +0530

    Add files via upload

---
+++ Python Casting.py
@@ -0,0 +1,31 @@
+#Python Casting
+#float()
+a = float(1)     # a will be 1.0
+b = float(6.3)   # b will be 6.3
+c = float("3")   # c will be 3.0
+d = float("1.6") # d will be 1.6
+
+print(a)
+print(b)
+print(c)
+print(d)
+
+#int()
+
+x = int(1)     # x will be 1
+y = int(2.3)   # y will be 2
+z = int("630") # z will be 630
+
+print(x)
+print(y)
+print(z)
+
+#str()
+
+p = str("L1") # x will be 'L1'
+q = str(6)    # y will be '6'
+r = str(3.0)  # z will be '3.0'
+
+print(p)
+print(q)
+print(r)
