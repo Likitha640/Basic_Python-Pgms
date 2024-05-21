ac43e98dfda7e3137ba05ada9170d4d7cfdb3c09
Likitha Rapuri <61692499+Likitha640@users.noreply.github.com>
2024-05-21 20:02:56 +0530

    Add files via upload

---
+++ Slicing Strings.py
@@ -0,0 +1,28 @@
+#Slicing Strings
+a = "Hello, World!"
+print(a[2:8])
+
+#Slice from the Start
+print(a[:5])
+
+#Slice from the End
+print(a[3:])
+
+#Negative Indexing
+print(a[-5:-2])
+
+#Modify Strings
+#upper()
+print(a.upper())
+
+#lower()
+print(a.lower())
+
+#remove white space
+print(a.strip())
+
+#replace
+print(a.replace("H","J"))
+
+#Split - Substrings
+print(a.split(","))
