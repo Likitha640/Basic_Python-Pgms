df619a72364485ce0a7f3b78f4df1a40c6033da5
Likitha Rapuri <61692499+Likitha640@users.noreply.github.com>
2024-05-24 19:43:01 +0530

    Python Lists.py

---
+++ Python Lists.py
@@ -0,0 +1,24 @@
+#Python Numbers
+x=3
+y=4.5
+z=3+2j
+print(type(x))
+print(type(y))
+print(type(z))
+
+#Type Conversion int to float
+a=float(x)
+print(a)
+print(type(a))
+
+#Type Conversion float to int
+b=int(y)
+print(b)
+print(type(b))
+
+#Type Conversion float to complex
+c=complex(y)
+print(c)
+print(type(c))
+
+#You cannot convert complex numbers into another number type.
